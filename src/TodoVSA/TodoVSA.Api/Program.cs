72bac54ea287b8911ce2e7dc427facc6acceb9e9
its0mar <omarjadallah24@gmail.com>
2025-09-20 07:48:07 +0300

    added scalar

--- src/TodoVSA/TodoVSA.Api/Program.cs
+++ src/TodoVSA/TodoVSA.Api/Program.cs
@@ -1,3 +1,4 @@
+using Scalar.AspNetCore;
 using TodoVSA.Api.Features.Todos.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,24 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<CreateTodoHandler>();
 builder.Services.AddScoped<DeleteTodoHandler>();
 builder.Services.AddScoped<CompleteTodoHandler>();
 builder.Services.AddScoped<GetTodosHandler>();
 
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();   
-    app.UseSwaggerUI(options =>
+    app.MapScalarApiReference(opt =>
     {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
-        options.RoutePrefix = string.Empty;
+        opt.Title = "TodoVSA API";
+        opt.DarkMode = true;
+        opt.Theme = ScalarTheme.Mars;
+        opt.BaseServerUrl = "http://localhost:5035";
+
     });
 }
 
--- src/TodoVSA/TodoVSA.Api/obj/Debug/net9.0/TodoVSA.Api.AssemblyInfo.cs
+++ src/TodoVSA/TodoVSA.Api/obj/Debug/net9.0/TodoVSA.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoVSA.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d53f9b6559571e750000b8b85415b3ff7cf91972")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoVSA.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoVSA.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
